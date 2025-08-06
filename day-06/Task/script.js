1894bd124ea34ea26d29c9a6c2dc93bb013cc90f
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-08-06 17:15:26 +0600

    (day06) added task 2 find the maxium of two numbers

--- day-06/Task/script.js
+++ day-06/Task/script.js
@@ -9,5 +9,21 @@ Formula: (Celsius * 9/5) + 32 = Fahrenheit
 function celsiusToFahrenheit(celsius) {
   return (celsius * 9) / 5 + 32;
 }
-const result1 = celsiusToFahrenheit(60);
-console.log(result1);
+console.log(celsiusToFahrenheit(60));
+
+/* 
+
+## 2. Create a Function to Find the Maximum of Two Numbers
+Write a function findMax(num1, num2) that returns the larger of the two numbers. It should work for negative numbers as well.
+
+*/
+
+function findMax(num1, num2) {
+  if (num1 > num2) {
+    return "The max number is " + num1;
+  } else {
+    return "The max number is " + num2;
+  }
+}
+
+console.log(findMax(-10, -9));
